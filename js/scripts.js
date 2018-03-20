f16afa3bc6d52d3ff02544d8b0b96b04f3d3a75e
Austin Roberts and Justin Roller <austin@>
2018-03-20 10:11:04 -0700

    add buttons for dark or light background, boostrap, script.js, and styles.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+
+
+
+
+
+
+
+});
